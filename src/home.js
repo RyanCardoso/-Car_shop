5bec66e58d1a89c8cdf954aea67692bca776649c
RyanCardoso <ryanferreiracardoso864@gmail.com>
2021-08-20 08:35:28 -0300

    Creating trash to remove everything

--- src/home.js
+++ src/home.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import * as S from './styles';
 
 //Components
-import CarImg from '../src/Assets/car.svg';
+import CarImg from './Assets/car.svg';
+import Bin from './Assets/bin.png';
 
 class Home extends Component {
     state = {
@@ -89,29 +90,41 @@ class Home extends Component {
 
         filter.isDisplay = false;
         this.setState({ carsAdd: this.state.carsAdd.concat(filter) })
+
     }
 
-    removeCar(ev, id) {
-        ev.preventDefault();
+    removeCar = (id) => {
         const { cars } = this.state;
         const filter = cars.find(item => item.id === id)
-        filter.isDisplay = true;
 
+        filter.isDisplay = true;
         this.setState({
             carsAdd: this.state.carsAdd.filter(item => item.id !== id)
         })
     }
 
 
+    clearAll = () => {
+        this.state.cars.map(item => item.isDisplay = true)
+
+        this.setState({
+            carsAdd: []
+        })
+    }
+
     drop = (e) => {
         e.preventDefault();
+        const target = e.target;
         const card_id = e.dataTransfer.getData('card_id');
 
-        const filter = this.state.cars.find(item => {
+        /* const filter = this.state.cars.find(item => {
             return item.id === Number(card_id)
-        })
+        }) */
 
-        this.addCar(filter.id);
+        if (target.id === 'boxSelect')
+            this.addCar(Number(card_id));
+        else if (target.id === 'boxItems')
+            this.removeCar(Number(card_id))
     }
 
     dragOver = (e) => {
@@ -124,12 +137,51 @@ class Home extends Component {
     }
 
 
-    renderCart = () => {
+    renderCar = () =>
+        this.state.cars.map((item, index) => (
+            <S.BoxCar
+                key={index}
+                id={item.id}
+                draggable={true}
+                onDragStart={this.dragStart}
+                check={item.isDisplay}
+            >
+                <S.BoxTitleCar>
+                    <S.TitleCar>{item.name}</S.TitleCar>
+                    <S.Add onClick={() => { this.addCar(item.id) }}>+</S.Add>
+                </S.BoxTitleCar>
+
+                <S.BoxInfoCar>
+                    <S.InfoCar>
+                        <span>Montadora: </span>
+                        {item.carMaker}
+                    </S.InfoCar>
+
+                    <S.InfoCar>
+                        <span>Preço: </span>
+                        {item.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+                    </S.InfoCar>
+
+                    <S.InfoCar>
+                        <span>Tipo: </span>
+                        {item.type}
+                    </S.InfoCar>
+                </S.BoxInfoCar>
+            </S.BoxCar>
+        ))
+
+
+    renderCarAdd = () => {
         return this.state.carsAdd.map((item, index) =>
-            <S.ItemsAdd key={index}>
+            <S.ItemsAdd
+                key={index}
+                id={item.id}
+                draggable={true}
+                onDragStart={this.dragStart}
+            >
                 <S.HeaderTitle>
                     <S.TitleCar color='white'>{item.name}</S.TitleCar>
-                    <S.Remove onClick={(ev) => { this.removeCar(ev, item.id) }}>-</S.Remove>
+                    <S.Remove onClick={() => this.removeCar(item.id)}>-</S.Remove>
                 </S.HeaderTitle>
                 <S.ItemInfo>
                     <S.InfoCar>
@@ -149,62 +201,45 @@ class Home extends Component {
     render() {
         return (
             <S.Container>
-                <S.BoxShop>
-                    {this.state.cars.map((item, index) => (
-                        <S.BoxCar
-                            key={index}
-                            id={item.id}
-                            draggable={true}
-                            onDragStart={this.dragStart}
-                            onDragOver={this.dragOver}
-                            check={item.isDisplay}
-                        >
-                            <S.BoxTitleCar>
-                                <S.TitleCar>{item.name}</S.TitleCar>
-                                <S.Add onClick={() => { this.addCar(item.id) }}>+</S.Add>
-                            </S.BoxTitleCar>
-
-                            <S.BoxInfoCar>
-                                <S.InfoCar>
-                                    <span>Montadora: </span>
-                                    {item.carMaker}
-                                </S.InfoCar>
-
-                                <S.InfoCar>
-                                    <span>Preço: </span>
-                                    {item.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
-                                </S.InfoCar>
-
-                                <S.InfoCar>
-                                    <span>Tipo: </span>
-                                    {item.type}
-                                </S.InfoCar>
-                            </S.BoxInfoCar>
-                        </S.BoxCar>
-                    ))}
-                </S.BoxShop>
-
-                <S.BoxSelect>
-                    <S.BoxItems
-                        id='board-2'
+                <S.BoxTitleStore>
+                    <S.TitleStore>Loja de Carros!</S.TitleStore>
+                </S.BoxTitleStore>
+                <S.ContainerStore>
+                    <S.BoxShop
+                        id='boxItems'
                         onDrop={(e) => { this.drop(e) }}
                         onDragOver={this.dragOver}
                     >
-                        {this.state.carsAdd.length ? this.renderCart() : <S.BoxPlaceholder>
-                            <S.CarImg src={CarImg} alt="Carro" />
-                            <S.Placeholder>Arraste seus carros preferidos aqui :)</S.Placeholder>
-                        </S.BoxPlaceholder>}
+                        {this.renderCar()}
+                    </S.BoxShop>
 
-                    </S.BoxItems>
+                    <S.BoxSelect>
+                        <S.BoxItems
+                            id='boxSelect'
+                            onDrop={(e) => { this.drop(e) }}
+                            onDragOver={this.dragOver}
+                        >
+                            {this.state.carsAdd.length ? this.renderCarAdd() : <S.BoxPlaceholder id='boxSelect'>
+                                <S.CarImg src={CarImg} alt="Carro" />
+                                <S.Placeholder>Arraste seus carros preferidos aqui :)</S.Placeholder>
+                            </S.BoxPlaceholder>}
 
-                    <S.BoxResult>
-                        <S.TitleCar size='1.5rem'>Total</S.TitleCar>
-                        <S.InfoCar size='1.5rem'>
-                            {this.state.carsAdd.reduce((a, b) => a + b.price, 0)
-                                .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
-                        </S.InfoCar>
-                    </S.BoxResult>
-                </S.BoxSelect>
+                        </S.BoxItems>
+
+                        <S.BoxResult>
+                            <S.TitleCar size='1.5rem'>Total</S.TitleCar>
+                            <S.InfoCar size='1.5rem'>
+                                {this.state.carsAdd.reduce((a, b) => a + b.price, 0)
+                                    .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+                            </S.InfoCar>
+                            <S.BoxClear onClick={this.clearAll}>
+                                <S.QuantityItems>{this.state.carsAdd.length}</S.QuantityItems>
+                                <S.ClearItems src={Bin} alt='Icon Lixeira' />
+                                <S.Popups class="popuptext" id="myPopup">Remover tudo</S.Popups>
+                            </S.BoxClear>
+                        </S.BoxResult>
+                    </S.BoxSelect>
+                </S.ContainerStore>
             </S.Container>
         )
     }
--- src/styles.js
+++ src/styles.js
@@ -1,12 +1,29 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const Container = styled.div`
     width: 95%;
-    min-height: 100vh;
     margin: 0 auto;
+    
+`;
+
+export const BoxTitleStore = styled.div`
+    width: 100%;
+    height: 60px;
     display: flex;
+    align-items:center;
+    justify-content: center;
+`;
+
+export const TitleStore = styled.h1``;
+
+export const ContainerStore = styled.div`
+    width: 100%;
+    min-height: calc(100vh - 92px);
+    display: flex;
+    flex-wrap: wrap;
     align-items: flex-start;
     justify-content: space-between; 
+    margin-top: 2rem;
 `;
 
 export const BoxShop = styled.div`
@@ -174,4 +191,78 @@ export const BoxResult = styled.div`
     display: flex;
     justify-content: space-between;
     margin-top: .5rem;
+`;
+
+
+export const Popups = styled.div`
+    display: none;
+    width: 160px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    padding: 8px 0;
+    position: absolute;
+    z-index: 1;
+    bottom: 125%;
+    left: 50%;
+    margin-left: -80px;
+
+    &::after {
+        content: "";
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        margin-left: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: #555 transparent transparent transparent;
+    }
+
+`;
+
+export const QuantityItems = styled.div`
+    position:absolute;
+    top: 50%;
+    left: 65%;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transform: translate(0, -50%);
+    border-radius: 50%;
+    color: white;
+    background: #555;
+    overflow: hidden;
+
+    &::after{
+        content: '';
+        position: absolute;
+        top: 100%;
+        border-radius: 50% 50% 0 0;
+        width: 150%;
+        height: 120%;
+        background: rgb(203, 77, 77);;
+        z-index: -1;
+        transition: .5s;
+    }
+`;
+
+export const BoxClear = styled.div`
+    position: relative;
+    margin-right: 3rem;
+    cursor: pointer;
+
+    &:hover ${Popups} {
+        display: block;
+    }
+
+    &:hover ${QuantityItems} ::after{
+        top: 0;
+    }
+`;
+
+export const ClearItems = styled.img`
+    width: 28px;
 `;
